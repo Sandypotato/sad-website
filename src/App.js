84f67d768a44600340d554baad4486ced9b60ac6
Sandypotato <anderschoozq@gmail.com>
2025-05-21 09:24:36 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	src/sad-image.jpg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from './sad-image.jpg'
+
+
+function click(){
+  alert("Fuck You")
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Click the fucking button dumbass</h1>
+      <div className='container'>
+        <img src={image} className="sad" alt="logo" />
+
+      </div>
+      <button onClick={click}>Are you sad?</button>
     </div>
   );
 }
